425ad02aa6d7deca8d45cfeb5240e09085fb941a
arthurmaia732-beep <arthurmaia732@gmail.com>
2026-07-02 23:50:52 -0400

    feat(backend): install entity framework, postgresql, and swagger packages

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,17 +1,23 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// Configura o Swagger/OpenAPI (Usando Swashbuckle)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    // Ativa o middleware para servir o JSON gerado e a UI do Swagger
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Minha API v1");
+    });
 }
 
 app.UseHttpsRedirection();
